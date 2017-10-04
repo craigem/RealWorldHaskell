c69046adf3b7f597e81bec0d954b53f926dbcd37
Craige McWhirter <craige@mcwhirter.io>
2017-10-04 17:08:58 +1000

    Modified to show a word count instead

--- ch01/WC.hs
+++ ch01/WC.hs
@@ -1,5 +1,5 @@
 -- file: ch01/WC.hs
 -- lines beginning with "--" are coments.
 
-main  = interact wordCount
-    where wordCount input = show (length (lines input)) ++ "\n"
+main = interact wordCount
+    where wordCount input = show (length (words input)) ++ "\n"
