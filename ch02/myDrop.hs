cb9d8e78b6bdfe4d4f7897ac0522bc5ea5a009ad
Craige McWhirter <craige@mcwhirter.io>
2017-10-06 13:30:07 +1000

    Added a type signature

--- ch02/myDrop.hs
+++ ch02/myDrop.hs
@@ -2,6 +2,7 @@
 {- The authors use this re-write of the "drop" function as an example of using
 Haskell's "if" conditional. -}
 
+myDrop :: Int -> [a] -> [a]
 myDrop n xs = if n <= 0 || null xs
     then xs
     else myDrop (n - 1) (tail xs)
