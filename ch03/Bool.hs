6e8dfb61ecca3bccca30ab061fee37735a5a1a7d
Craige McWhirter <craige@mcwhirter.io>
2017-10-11 18:07:13 +1000

    Added Bool.hs

---
+++ ch03/Bool.hs
@@ -0,0 +1,7 @@
+-- file: ch03/Bool.hs
+
+{- Bool is the simplest common example of a category of type called an
+algebraic data type. An algebraic data type can have more than one value
+constructor. -}
+
+data Bool = False | True
