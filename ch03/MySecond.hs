0b42074dfb705825ad489871927a4e4170bfdc64
Craige McWhirter <craige@mcwhirter.io>
2017-10-17 11:37:10 +1000

    A More Controlled Approach to Reporting Errors

--- ch03/MySecond.hs
+++ ch03/MySecond.hs
@@ -6,3 +6,17 @@ mySecond :: [a] -> a
 mySecond xs = if null (tail xs)
               then error "list too short"
               else head (tail xs)
+
+-- A more controlled approach to error handling
+safeSecond :: [a] -> Maybe a
+
+safeSecond [] = Nothing
+safeSecond xs = if null (tail xs)
+                then Nothing
+                else Just (head (tail xs))
+
+-- Making it even better with pattern matching
+tidySecond :: [a] -> Maybe a
+
+tidySecond (_:x:_) = Just x
+tidySecond _       = Nothing
