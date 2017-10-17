cad16769c9bea330c44de084d7dcacfc4bb989a4
Craige McWhirter <craige@mcwhirter.io>
2017-10-17 20:23:24 +1000

    The Offside Rule and Whitespace in an Expression

---
+++ ch03/GoodIndent.hs
@@ -0,0 +1,9 @@
+-- file: ch03/goodIndent.hs
+
+-- This is the left most column
+
+  -- It's fine for top-level delcarations to start in any column...
+  firstGoodIndentation = 1
+
+  -- ...provided all subsequent declarations do, too!
+  secondGoodIndentation = 2
