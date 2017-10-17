03e88413bfbd0862632b85e17bea651653cf6e61
Craige McWhirter <craige@mcwhirter.io>
2017-10-17 12:00:12 +1000

    The where Clause

--- ch03/Lending.hs
+++ ch03/Lending.hs
@@ -7,3 +7,10 @@ lend amount balance = let reserve    = 800
                          then Nothing
                          else Just newBalance
 
+-- Re-writing lend using where instead of let. where applies to preceding code
+lend2 amount balance = if amount < reserve * 0.5
+                       then Just newBalance
+                       else Nothing
+    where reserve    = 100
+          newBalance = balance - amount
+
